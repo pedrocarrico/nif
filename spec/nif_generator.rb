d3aa78c494575ceb7e759d618113d53a8b278b88
Pedro Carriço <pedro.carrico@yunit.pt>
2012-03-03 20:49:40 +0000

    Refactor and add rspec tests

--- spec/nif_generator.rb
+++ spec/nif_generator.rb
@@ -1,11 +1,27 @@
 require 'lib/nif_generator'
 
 describe "NifGenerator" do
-  it "should validade a valid Nif" do
+  it "should validate a valid nif" do
     valid_nif = '502874210'
     nif_generator = NifGenerator.instance
-    result = NifGenerator.instance.validate(valid_nif)
+    result = nif_generator.validate(valid_nif)
 
     result.should == true    
   end
+
+  it "should not validate an invalid nif" do
+    invalid_nif = '502874211'
+    nif_generator = NifGenerator.instance
+    result = nif_generator.validate(invalid_nif)
+
+    result.should == false    
+  end
+  
+  it "should generate a valid nif" do
+    nif_generator = NifGenerator.instance
+    nif = nif_generator.generate
+    result = nif_generator.validate(nif)
+
+    result.should == true    
+  end    
 end
