59c2de8cf848a6218ac13401554bb119e4d48bfd
Pedro Carriço <pedro.carrico@gmail.com>
2012-03-31 13:10:17 +0100

    Refactor specs

--- spec/nif_generator_spec.rb
+++ spec/nif_generator_spec.rb
@@ -1,9 +1,10 @@
 require 'lib/nif_generator'
 
 describe "NifGenerator" do
+  let(:nif_generator) { NifGenerator.instance }
+
   it "should validate a valid nif" do
     valid_nif = '502874210'
-    nif_generator = NifGenerator.instance
     result = nif_generator.validate(valid_nif)
 
     result.should == true    
@@ -11,14 +12,12 @@ describe "NifGenerator" do
 
   it "should not validate an invalid nif" do
     invalid_nif = '502874211'
-    nif_generator = NifGenerator.instance
     result = nif_generator.validate(invalid_nif)
 
     result.should == false    
   end
   
   it "should generate a valid nif" do
-    nif_generator = NifGenerator.instance
     nif = nif_generator.generate
     result = nif_generator.validate(nif)
 
